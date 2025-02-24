2cc14aa2b9a37033f06967575e07fd2c5f9930fb
philip200720 <aposadas-2023357@kinal.edu.gt>
2025-02-24 17:26:09 -0600

    initial config

---
+++ configs/swagger.js
@@ -0,0 +1,30 @@
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express"
+
+const swaggerOptions = {
+    swaggerDefinition:{
+        openapi: "3.0.0",
+        info:{
+            title: "Coperex API",
+            version:"1.0.0",
+            description: "Coperex API",
+            contact:{
+                name: "Philip Posadas",
+                email: "aposadas-2023357@kinal.edu.gt"
+            }
+        },
+        servers:[
+            {
+                url: "http://127.0.0.1:3000/coperex/v1/"
+            }
+        ]
+    },
+    /*
+    apis:[
+    ]
+    */
+}
+
+const swaggerDocs = swaggerJSDoc(swaggerOptions)
+
+export { swaggerDocs, swaggerUi }
